a4ac2d74d52e63a09223e5164d36ab69337d42e8
arjun <arjun.mehta1001@gmail.com>
2021-12-25 16:39:04 -0700

    information updated

--- src/components/Division.js
+++ src/components/Division.js
@@ -18,9 +18,8 @@ import logo from '../logo.svg';
 
 const Division = props => {
 
-    const mediaSet = {'aboutMe': [<Round code='0' />, <div><p>I'm a third year computer software engineering student at the University of Alberta. In my spare time you can find me in the U of A engineering quad (pretending to study) or at the gym
-                        {/* I like trying new things and am currently getting into nature photography (pics to come soon).*/}
-                        . My favourite part about software engineering is that it allows me to be creative in solving new and challenging problems.
+    const mediaSet = {'aboutMe': [<Round code='0' />, <div><p>
+                            I'm a third year computer software engineering student at the University of Alberta.  In my spare time you can find me in the U of A engineering quad (pretending to study) or at the gym. My favourite part of software engineering is that it allows me to be creative in solving new and challenging problems.
                         </p>
                         <ButtonPair />
                         </div>],
@@ -28,11 +27,12 @@ const Division = props => {
 
                     'skills': [<div>
                         <h2><b>Technical Skills</b></h2>
-                        <p>Some self serving bs</p><br/>
                         <h3>Languages</h3>
                         <p>Python, C, C++, Java, MATLAB, JavaScript, HTML, CSS, VHDL</p>
                         <h3>Frameworks/Packages</h3>
-                        <p>React.js, Node.js, Pandas, Beautiful Soup</p>  
+                        <p>React.js, Node.js, Pandas, Beautiful Soup, Selenium, Bootstrap</p>  
+                        <br/>
+                        <p>I also have experience working with Google Firebase for personal and professional projects.</p>
                     </div>,
                     <img src={logo} className="App-logo" alt="logo" />],
 
@@ -41,7 +41,8 @@ const Division = props => {
                         <h2>
                             <b>What's Cookin'?</b>
                         </h2>
-                        <p>How do i phrase this to say i do a lot and this is what I want to highlight?</p>
+                        <p>I’m always looking to apply my skill set in new and interesting ways. I’m currently learning about the fundamentals of blockchain with the aim to launch my own cryptocurrency. <br/><br/>I’m also working to develop my nature photography skills as a side hobby. 
+</p>
                     </div>]
                         };
 
